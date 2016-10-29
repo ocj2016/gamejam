f2545851ba431e921ead157c1911ef78be010ca7
Tyler Weeres <tweeres04@gmail.com>
2016-10-28 22:14:56 -0700

    webpack setup

---
+++ main.js
@@ -0,0 +1,20 @@
+import 'file?name=[name].[ext]!phaser/build/phaser.min';
+import 'file?name=[name].[ext]!./index.html';
+
+const game = new Phaser.Game(800, 600, Phaser.AUTO, 'quidditch', {
+	preload,
+	create
+});
+
+function preload(){
+
+}
+
+function create(){
+	const text = 'Hello gamejam';
+	const style = {
+		fill: '#ffffff'
+	}
+	
+	const t = game.add.text(game.world.centerX-300, 0, text, style);
+}
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: './bin',
+		filename: 'bundle.js'
+	},
+	devtool: 'eval-source-map',
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+		]
+	},
+	devServer: {
+		contentBase: 'public'
+	}
+};
