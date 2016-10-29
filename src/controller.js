c99db9fca732f8c947fdc2a921d214768c806484
Tyler Weeres <tweeres04@gmail.com>
2016-10-29 13:41:02 -0700

    Fixed indentation; removed some comments

--- src/controller.js
+++ src/controller.js
@@ -1,45 +1,33 @@
-// // creation of a new AirConsole instance
-// var airconsole = new AirConsole();
-// // function to be executed every 3 seconds
-// setInterval(function(){
-//      // generating a random value
-//      var value = Math.floor(Math.random() * 100);
-//      // this is how we send a message to AirConsole main screen
-//      airconsole.message(AirConsole.SCREEN, value);  
-//      // updating "value" h1 content to show on the controller the value we are sending
-//      document.getElementById("value").innerHTML = "I am sending " + value;   
-// }, 3000);
-
-    navigator.vibrate = (navigator.vibrate ||
-                         navigator.webkitVibrate ||
-                         navigator.mozVibrate ||
-                         navigator.msVibrate);
-    var airconsole;
-    /**
-     * Sets up the communication to the screen.
-     */
-    window.onload = () => {
-        airconsole = new AirConsole({"orientation": "landscape"});
-        /*
-        * Checks if this device is part of the active game.
-        */
-        airconsole.onActivePlayersChange = function(player) {
-            var div = document.getElementById("player_id");
-            if (player !== undefined) {
-                div.innerHTML =  (["Left Player", "Right Player"][player]);
-            } else {
-                div.innerHTML = "It's a 2 player game!";
-            }
-        };
-        /*
-        * Makes the device vibrate if the screen says so.
-        */
-        airconsole.onMessage = function(from, data) {
-            if (from == AirConsole.SCREEN && data.vibrate) {
-                navigator.vibrate(data.vibrate);
-                console.log("Vibrating: " + data.vibrate);
-            }
-        };
+navigator.vibrate = (navigator.vibrate ||
+                     navigator.webkitVibrate ||
+                     navigator.mozVibrate ||
+                     navigator.msVibrate);
+var airconsole;
+/**
+ * Sets up the communication to the screen.
+ */
+window.onload = () => {
+    airconsole = new AirConsole({"orientation": "landscape"});
+    /*
+    * Checks if this device is part of the active game.
+    */
+    airconsole.onActivePlayersChange = function(player) {
+        var div = document.getElementById("player_id");
+        if (player !== undefined) {
+            div.innerHTML =  (["Left Player", "Right Player"][player]);
+        } else {
+            div.innerHTML = "It's a 2 player game!";
+        }
+    };
+    /*
+    * Makes the device vibrate if the screen says so.
+    */
+    airconsole.onMessage = function(from, data) {
+        if (from == AirConsole.SCREEN && data.vibrate) {
+            navigator.vibrate(data.vibrate);
+            console.log("Vibrating: " + data.vibrate);
+        }
+    };
 
     function move(amount) {
         var toMove = amount;
@@ -65,7 +53,4 @@
 
     document.getElementById("div-dash").addEventListener("touchstart", dash, false);
     document.getElementById("div-dash").addEventListener("mousedown", dash, false);
-
-    }
-
-
+}
